f05127282566a944d77b8d43596085811440c3ae
Saad <saad.h@valuebound.com>
2021-10-08 04:43:55 +0530

    added async await

--- categories/categories.js
+++ categories/categories.js
@@ -1,49 +1,47 @@
 var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/ecom";
-MongoClient.connect(url, function (err, client) {
-    if (err) throw err;
-    var myobj = [
-        { Name: "home appliance", Slug : "https//amazon.in/homeAppliance", Image: "#pic", Description:"#about" },
-        { Name: "kitchen appliance", Slug : "https//amazon.in/kitchen", Image: "#pic", Description:"#about"},
-        { Name: "mobile", Slug: "https//amazon.in/mob", Image: "#pic", Description:"#about"},
-        { Name: "laptop", Slug: "https//amazon.in/laptop",Image: "#pic", Description:"#about"},
-        { Name: "fashion", Slug: "https//amazon.in/fashion",Image: "#pic", Description:"#about" },
-        { Name: "book", Slug: "https//amazon.in/book",Image:"#pic",Description:"#about"  },
-        { Name: "furniture", Slug: "https//amazon.in/furniture", Image: "#pic", Description:"#about" },
-    ];
-    const db = client.db("ecom");
+var url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const dbName = "ecom";
+
+
+var data = [
+    { Name: "home appliance", Slug: "https//amazon.in/homeAppliance", Image: "#pic", Description: "#about" },
+    { Name: "kitchen appliance", Slug: "https//amazon.in/kitchen", Image: "#pic", Description: "#about" },
+    { Name: "mobile", Slug: "https//amazon.in/mob", Image: "#pic", Description: "#about" },
+    { Name: "laptop", Slug: "https//amazon.in/laptop", Image: "#pic", Description: "#about" },
+    { Name: "fashion", Slug: "https//amazon.in/fashion", Image: "#pic", Description: "#about" },
+    { Name: "book", Slug: "https//amazon.in/book", Image: "#pic", Description: "#about" },
+    { Name: "furniture", Slug: "https//amazon.in/furniture", Image: "#pic", Description: "#about" },
+];
+
+async function main() {
+    let query;
+    await client.connect();
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
 
     // create
-    db.collection("Categories").insertMany(myobj, function (err, res) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + res.insertedCount);
-
-    });
+    const insertData = await db.collection("categories").insertMany(data);
+    console.log(insertData.insertedCount + "record inserted");
 
     // read
-    var query = { Name: "mobile" };
-    db.collection("Categories").find(query).toArray(function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
-
+    query = { Name: "kitchen appliance"};
+    const getcategories = await db.collection("categories").find(query).toArray();
+    console.log(getcategories);
 
     // update
-    db.collection("Categories").updateOne({ Name: "book" }, { $set: { Name: "Pustak" } }, function (err, res) {
-        if (err) throw err;
-        console.log("updated");
-
-    });
-
+    query = { Name: "mobile" };
+    const updatequery = { $set: { Name: "mobile phone" } };
+    const updateCategories = await db.collection("categories").updateMany(query, updatequery);
+    console.log(updateCategories.modifiedCount + " record updated");
 
     // delete
-    var myquery = { Name: 'fashion' };
-    db.collection("Categories").deleteOne(myquery, function (err, obj) {
-        if (err) throw err;
-        console.log(obj.result + " record(s) deleted");
-        client.close();
+    query = { Name: "kitchen appliance" };
+    const deleteCategories = await db.collection("categories").deleteMany(query);
+    console.log(deleteCategories.deletedCount + " record deleted");
+}
 
-    });
-
-
-});
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
